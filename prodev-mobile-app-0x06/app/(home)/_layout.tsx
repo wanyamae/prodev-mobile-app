b811ed9809571099ff46ff11cdfb2f6fed813bc7
Emmanuel Wanyama <160263746+wanyamae@users.noreply.github.com>
2025-09-05 20:54:53 +0300

    feat: Remove headerShown option from Tabs screen options in HomeRootLayout

--- prodev-mobile-app-0x06/app/(home)/_layout.tsx
+++ prodev-mobile-app-0x06/app/(home)/_layout.tsx
@@ -13,22 +13,18 @@ const HomeRootLayout = () => {
       }} />
       <Tabs.Screen name="search" options={{
         title: 'Search',
-        headerShown: true,
         tabBarIcon: ({ color }) => <Feather name="search" size={24} color={color} />,
       }} />
       <Tabs.Screen name="saved" options={{
         title: 'Saved',
-        headerShown: true,
         tabBarIcon: ({ color }) => <EvilIcons name="heart" size={27} color={color} />
       }} />
         <Tabs.Screen name="inbox" options={{
         title: 'Inbox',
-        headerShown: true,
         tabBarIcon: ({ color }) => <Ionicons name="chatbubbles-outline" size={24} color={color} />
       }} />
         <Tabs.Screen name="profile" options={{
         title: 'Profile',
-        headerShown: true,
         tabBarIcon: ({ color }) => <FontAwesome name="user-o" size={24} color="black" />
       }} />
     </Tabs>
